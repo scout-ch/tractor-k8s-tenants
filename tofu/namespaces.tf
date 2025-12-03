c6aca828b4ae370b9a3812688524148338c20c68
DominikFischli <dominikfischli@bluewin.ch>
2025-12-03 10:35:11 +0100

    feat: pfadi-finder-midata-adapter namespace

--- tofu/namespaces.tf
+++ tofu/namespaces.tf
@@ -27,6 +27,17 @@ module "ns_pbs_who_is_who" {
   }
 }
 
+module "ns_pbs_pfadi_finder_midata_adapter" {
+  source = "./modules/namespace"
+
+  name          = "pbs-pfadi-finder-midata-adapter"
+  instance_pool = "pck-8kxhclv-pdp"
+
+  users = {
+    "floh-pfadi-finder-midata-adapter" = module.u_floh.unique_username
+  }
+}
+
 module "ns_mova_erinnerungsbuch" {
   source = "./modules/namespace"
 
