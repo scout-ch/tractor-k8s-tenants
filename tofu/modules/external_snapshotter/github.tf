bc4da930da1d89ba0f06c1de840da9694d96a057
Nicolas Da Mutten <nicolas@damutten.ch>
2026-06-09 00:09:14 +0200

    fix: separate volumesnapshotclass into own kustomization

--- tofu/modules/external_snapshotter/github.tf
+++ tofu/modules/external_snapshotter/github.tf
@@ -3,3 +3,11 @@ resource "github_repository_file" "this" {
   file       = "infrastructure/external-snapshotter/external-snapshotter.yaml"
   content    = file("${path.module}/resources/external-snapshotter.yaml")
 }
+
+resource "github_repository_file" "volume_snapshot_class" {
+  repository = var.github_repository
+  file       = "infrastructure/volume-snapshot-class/volume-snapshot-class.yaml"
+  content    = file("${path.module}/resources/volume-snapshot-class.yaml")
+
+  depends_on = [github_repository_file.this]
+}
