3c153e16fd71b572569759c24c0f92dda0982ce5
Nicolas Da Mutten <nicolas@damutten.ch>
2026-06-17 00:12:27 +0200

    fix: kyverno cleanup

--- tofu/modules/kyverno/github.tf
+++ tofu/modules/kyverno/github.tf
@@ -9,11 +9,3 @@ resource "github_repository_file" "instance_pool_selector_policy" {
   file       = "infrastructure/kyverno-policies/instance-pool-selector/instance-pool-selector.yaml"
   content    = file("${path.module}/resources/instance-pool-selector-policy.yaml")
 }
-
-resource "github_repository_file" "instance_pool_selector_policy_kustomization" {
-  repository = var.github_repository
-  file       = "infrastructure/kyverno-policies/instance-pool-selector/kustomization.yaml"
-  content = templatefile("${path.module}/resources/policy-kustomization.tftpl", {
-    policy_file = "./instance-pool-selector.yaml"
-  })
-}
